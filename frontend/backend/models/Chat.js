0567cbf555356ccdf4a33a7f0fa4ced76bfed556
venkatesh7975 <venkateshmoyya7975@gmail.com>
2025-07-14 03:08:49 +0530

    correct version

--- frontend/backend/models/Chat.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const chatSchema = new mongoose.Schema({
-  type: String, // "question" or "answer"
-  content: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("Chat", chatSchema);
